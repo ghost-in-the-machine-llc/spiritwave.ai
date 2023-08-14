19d2f55d6fd21b413a7073d8a76d56e4f7695562
Marty Nelson <marty@martypdx.com>
2023-08-14 15:42:04 -0700

    poc server stream

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,49 @@
-exports.handler = async function () {
+const { stream } = require('@netlify/functions');
+
+const API_KEY = process.env.OPENAI_API_KEY;
+
+exports.handler = stream(async (event) => {
+    // Get the request from the request query string, or use a default
+    const pie =
+        event.queryStringParameters?.pie ??
+        'something inspired by a springtime garden';
+
+    // The response body returned from "fetch" is a "ReadableStream",
+    // so you can return it directly in your streaming response
+    const res = await fetch(
+        'https://api.openai.com/v1/chat/completions',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Set this environment variable to your own key
+                Authorization: `Bearer ${API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        role: 'system',
+                        content:
+                            'You are a baker, describe a kind of pie that matches the users input in 120 characters or less',
+                    },
+                    // Use "slice" to limit the length of the input to 500 characters
+                    { role: 'user', content: pie.slice(0, 500) },
+                ],
+                // Use server-sent events to stream the response
+                stream: true,
+            }),
+        }
+    );
+
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Hello World' }),
+        headers: {
+            // This is the mimetype for server-sent events
+            'content-type': 'text/event-stream',
+            // 'content-type': 'application/json',
+        },
+        statusCode: res.status,
+        // Pipe the event stream from OpenAI to the client
+        body: res.body,
     };
-};
+});
--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
-import('./players.js');
-import('./support-us.js');
+// import('./support-us.js');
+
+const API = '/.netlify/functions/hello';
+
+const eventSource = new EventSource(API);
+
+const addListener = (name) =>
+    eventSource.addEventListener(name, (e) => {
+        // console.log(name, e.data);
+        if (name !== 'message') return console.log(name, e.data);
+        if (e.data === '[DONE]') {
+            return eventSource.close();
+        }
+        const message = JSON.parse(e.data);
+        console.log(message.choices[0].delta.content);
+    });
+
+addListener('message');
+addListener('open');
+addListener('error');
+
+// const res = await fetch(API);
+// console.log(await res.json());
