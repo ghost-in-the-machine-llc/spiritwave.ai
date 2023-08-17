39a12ce1cdcfd5570e75c92d5c2458e0239716bf
Marty Nelson <marty@martypdx.com>
2023-08-17 12:22:03 -0700

    updated server-side hello function with streaming

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,6 +2,36 @@ const { stream } = require('@netlify/functions');
 
 const API_KEY = process.env.OPENAI_API_KEY;
 
+class OpenAIContentStream extends TransformStream {
+    static contentRegEx =
+        /data: {.+"delta":{"content":"(.+)"}/gm;
+
+    constructor() {
+        super({
+            transform(chunk, controller) {
+                const matches = chunk.matchAll(
+                    OpenAIContentStream.contentRegEx
+                );
+                for (const [, content] of matches) {
+                    controller.enqueue(content);
+                }
+            },
+        });
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+class LogStdOutStream extends TransformStream {
+    constructor() {
+        super({
+            transform(chunk, controller) {
+                process.stdout.write(chunk);
+                controller.enqueue(chunk);
+            },
+        });
+    }
+}
+
 exports.handler = stream(async (event) => {
     // Get the request from the request query string, or use a default
     const pie =
@@ -25,7 +55,7 @@ exports.handler = stream(async (event) => {
                     {
                         role: 'system',
                         content:
-                            'You are a baker, describe a kind of pie that matches the users input in 120 characters or less',
+                            'You are a baker, describe a kind of pie that matches the users input in 120 character and quote the pie name',
                     },
                     // Use "slice" to limit the length of the input to 500 characters
                     { role: 'user', content: pie.slice(0, 500) },
@@ -36,6 +66,11 @@ exports.handler = stream(async (event) => {
         }
     );
 
+    const stream = res.body
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(new OpenAIContentStream());
+    // .pipeThrough(new LogStdOutStream());
+
     return {
         headers: {
             // This is the mimetype for server-sent events
@@ -44,6 +79,6 @@ exports.handler = stream(async (event) => {
         },
         statusCode: res.status,
         // Pipe the event stream from OpenAI to the client
-        body: res.body,
+        body: stream,
     };
 });
