d998bee90e87f6eb48418b38c2c801c5bb658e87
Marty Nelson <marty@martypdx.com>
2023-08-18 14:40:13 -0700

    refactor server functions for openai

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,86 +1,24 @@
-const { stream } = require('@netlify/functions');
+import { stream } from '@netlify/functions';
+import { streamCompletion } from '../lib/openai.js';
 
-const API_KEY = process.env.OPENAI_API_KEY;
+export const handler = stream(async (event) => {
 
-class OpenAIContentStream extends TransformStream {
-    static contentRegEx = /data: {.+"delta":{"content":(".+")}/gm;
-
-    constructor() {
-        super({
-            transform(chunk, controller) {
-                const matches = chunk.matchAll(OpenAIContentStream.contentRegEx);
-                try {
-                    for (const [, group] of matches) {
-                        // parse the JSON to decode
-                        // encoded characters like \", \', etc.
-                        controller.enqueue(JSON.parse(group));
-                    }
-                } catch (err) {
-                    // eslint-disable-next-line no-console
-                    console.log(err);
-                }
-            },
-        });
-    }
-}
-
-// eslint-disable-next-line no-unused-vars
-class LogStdOutStream extends TransformStream {
-    constructor() {
-        super({
-            transform(chunk, controller) {
-                process.stdout.write(chunk);
-                controller.enqueue(chunk);
-            },
-        });
-    }
-}
-
-exports.handler = stream(async (event) => {
-    // Get the request from the request query string, or use a default
     const pie =
-        event.queryStringParameters?.pie ??
-        'something inspired by a springtime garden';
+    event.queryStringParameters?.pie ??
+    'zombie apocalypse';
 
-    // The response body returned from "fetch" is a "ReadableStream",
-    // so you can return it directly in your streaming response
-    const res = await fetch(
-        'https://api.openai.com/v1/chat/completions',
+    const messages = [
         {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${API_KEY}`,
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages: [
-                    {
-                        role: 'system',
-                        content:
-                            `You are a baker, create a name and describe a kind of pie 
-                            that matches the users input in 120 character.
-                            The provide 300 characters describing the ingredients.`,
-                    },
-                    // Use "slice" to limit the length of the input to 500 characters
-                    { role: 'user', content: pie.slice(0, 500) },
-                ],
-                temperature: 1.5,
-                stream: true,
-            }),
-        }
-    );
-
-    const stream = res.body
-        .pipeThrough(new TextDecoderStream())
-        .pipeThrough(new OpenAIContentStream());
-        // .pipeThrough(new LogStdOutStream());
-
-    return {
-        headers: {
-            'content-type': 'text/event-stream',
+            role: 'system',
+            content:
+                `You are a baker, create a name and describe a kind of pie 
+                that matches the users input in 120 character, quote the pie name.
+                Then provide 300 characters describing the ingredients.`,
         },
-        statusCode: res.status,
-        body: stream,
-    };
+        // Use "slice" to limit the length of the input to 500 characters
+        { role: 'user', content: pie.slice(0, 500) },
+    ];
+
+    return await streamCompletion(messages, { temperature: 1.5 });
+
 });
---
+++ netlify/lib/openai.js
@@ -0,0 +1,42 @@
+import { OpenAIContentStream } from './streams.js';
+
+const API_KEY = process.env.OPENAI_API_KEY;
+const COMPLETIONS_URL = 'https://api.openai.com/v1/chat/completions';
+
+export async function streamCompletion(messages, options) {
+
+    // The response body returned from "fetch" is a "ReadableStream",
+    // so you can return it directly in your streaming response
+    const res = await fetch(
+        COMPLETIONS_URL,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages,
+                temperature: 1.2, // TODO: what is default?
+                stream: true,
+                ...options
+            }),
+        }
+    );
+
+    // TODO: status code error is body or stream?
+
+    const stream = res.body
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(new OpenAIContentStream());
+        // .pipeThrough(new LogStdOutStream());
+
+    return {
+        headers: {
+            'content-type': 'text/event-stream',
+        },
+        statusCode: res.status,
+        body: stream,
+    };
+}
