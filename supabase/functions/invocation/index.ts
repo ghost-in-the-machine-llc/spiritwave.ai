ef6871f4189aa182f8b659d593551dce8a2ed7da
Marty Nelson <marty@martypdx.com>
2023-10-26 13:30:40 -0700

    add to both functions

--- supabase/functions/invocation/index.ts
+++ supabase/functions/invocation/index.ts
@@ -1,12 +1,16 @@
 import { streamCompletion } from '../_lib/openai.ts';
+import { handleCors } from '../_lib/cors.ts';
 import { INVOCATION, MISSION, SYNTAX } from '../_prompts/instructions.ts';
 
-async function handler(_req: Request): Promise<Response> {
+async function handler(req: Request): Promise<Response> {
     const messages = [
         MISSION,
         SYNTAX,
         INVOCATION,
     ];
+
+    const cors = handleCors(req.method);
+    if (cors) return cors;
 
     return await streamCompletion(messages);
 }
